a8b1d177ce0594e5c08ba467afce3d1cfe7d5b40
SijiaHou <housj1228@gmail.com>
2023-09-14 17:22:37 -0700

    Implement shooting in a maxRange

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -7,22 +7,38 @@ public class BulletScript : MonoBehaviour
     private Vector3 mousePos;
     private Camera mainCam;
     private Rigidbody2D rb;
+    private Vector3 initialPosition;
+    public float maxRange; // Maximum range the bullet can travel.
     public float force;
     void Start()
     {
+        // the initial position of the bullet
+        initialPosition = transform.position;
+
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePos - transform.position;
         // for bullets to rotate
-        Vector3 rotation = transform.position - mousePos;
+        // Vector3 rotation = transform.position - mousePos;
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
-        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+        // float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        // transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
     void Update()
     {
+        // Calculate the distance between the bullet's initial position and its current position.
+        // Calculate the distance squared between the bullet's initial position and its current position.
+        float distanceSquared = (transform.position - initialPosition).sqrMagnitude;
 
+        Debug.Log("Distance Squared: " + distanceSquared + " sprtMaxRange" + maxRange * maxRange);
+
+        // If the squared distance exceeds the squared maximum range, destroy the bulle
+        if (distanceSquared > maxRange * maxRange)
+        {
+            Debug.Log("Bullet Destroyed");
+            Destroy(gameObject);
+        }
     }
 }
