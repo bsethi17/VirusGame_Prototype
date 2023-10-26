ff1cde5b0a181b5c64712955773808ca553e1455
Freda32 <62142082+Freda32@users.noreply.github.com>
2023-10-26 00:40:29 -0700

    Add human speed change

---
+++ Assets/Scripts/CirclePathMovementNVLvl4.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CirclePathMovementNVLvl4 : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float timeToNextPoint = 2.0f;
+    private int currentWaypointIndex = 0;
+    private int direction = 1;
+
+    private float elapsedTime = 0.0f;
+
+    void Update()
+    {
+        if (waypoints.Length < 2) return;
+
+        Transform currentWaypoint = waypoints[currentWaypointIndex];
+        Transform nextWaypoint = waypoints[(currentWaypointIndex + direction + waypoints.Length) % waypoints.Length];
+
+        elapsedTime += Time.deltaTime;
+        float fractionOfJourney = elapsedTime / timeToNextPoint;
+
+        transform.position = Vector2.Lerp(currentWaypoint.position, nextWaypoint.position, fractionOfJourney);
+
+        if (fractionOfJourney >= 1.0f)
+        {
+            currentWaypointIndex = (currentWaypointIndex + direction + waypoints.Length) % waypoints.Length;
+            elapsedTime = 0.0f;
+
+            if (currentWaypointIndex == 0 || currentWaypointIndex == waypoints.Length - 1)
+            {
+                direction = -direction;
+            }
+        }
+        
+        Debug.Log("Current waypoint index: " + currentWaypointIndex);
+    }
+}
