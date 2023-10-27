2411c6bc3983e42962098a22cfdc33fa98d5eba3
Freda32 <62142082+Freda32@users.noreply.github.com>
2023-10-26 20:31:55 -0700

    Change shield, human path

--- Assets/Scripts/CirclePathMovementNV1.cs
+++ Assets/Scripts/CirclePathMovementNV1.cs
@@ -5,18 +5,23 @@ public class CirclePathMovementNV1 : MonoBehaviour
     public Transform[] waypoints;
     public float speed = 5.0f;
     private int currentWaypointIndex = 0;
+    private int direction = 1;  // 1 for forward, -1 for backward
 
     void Update()
     {
-        if (waypoints.Length == 0) return;
+        if (waypoints.Length < 2) return;  // Need at least 2 waypoints to move
 
         Transform targetWaypoint = waypoints[currentWaypointIndex];
         transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
 
         if (Vector2.Distance(transform.position, targetWaypoint.position) < 0.001f)
         {
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            currentWaypointIndex += direction;
+
+            if (currentWaypointIndex >= waypoints.Length - 1 || currentWaypointIndex <= 0)
+            {
+                direction = -direction;  // Change direction
+            }
         }
-        
     }
 }
