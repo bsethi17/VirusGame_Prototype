2ac403da8c3bc0ec9322268f7d0f7431c2191a7c
Sai Kumar <kasarla@usc.edu>
2023-11-01 17:57:22 -0700

    Added Menu Button Option - Sai Kumar Reddy Kasarla

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
+    private bool isPaused;
 
     // Start is called before the first frame update
     void Start()
@@ -15,16 +16,25 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused) {
+                ResumeGame();
+            }
+            else {
+                PauseGame();
+            }
+        }
     }
 
     public void PauseGame() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        isPaused = true;
     }
 
     public void ResumeGame() {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        isPaused = false;
     }
 }
