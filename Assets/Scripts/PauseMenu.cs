134caf76fc8aff222ba75f7b1aa053f630785287
Sai Kumar <kasarla@usc.edu>
2023-11-02 12:27:20 -0700

    Pause Functionality Fix - Sai Kumar Reddy Kasarla

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
-    private bool isPaused;
 
     // Start is called before the first frame update
     void Start()
@@ -16,25 +15,18 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (isPaused) {
-                ResumeGame();
-            }
-            else {
-                PauseGame();
-            }
+        if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale != 0f) {
+            PauseGame();
         }
     }
 
     public void PauseGame() {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
-        isPaused = true;
     }
 
     public void ResumeGame() {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
-        isPaused = false;
     }
 }
