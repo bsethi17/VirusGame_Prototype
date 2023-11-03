96a81ccc86bdef0feafe7dbac866f1df3fdf9914
bsethi17 <139799522+bsethi17@users.noreply.github.com>
2023-11-03 04:28:25 -0700

    got grenade quicker in shooting level

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RandomSpawner : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class RandomSpawner : MonoBehaviour
     public Transform[] spawnPoints;
     private GameObject currentGrenade;
     private GameObject currentShield;
+
+    string currentSceneName;
 
     private void Start()
     {
@@ -18,7 +21,16 @@ public class RandomSpawner : MonoBehaviour
     {
         while (true) // This will keep the routine running indefinitely
         {
-            yield return new WaitForSeconds(5f);  // Wait for 5 seconds
+            currentSceneName = SceneManager.GetActiveScene().name;
+            if (currentSceneName == "Level7")
+            {
+                yield return new WaitForSeconds(2f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(5f);
+            }
+              // Wait for 5 seconds
 
             // If there's already a grenade or shield in the scene, destroy them
             if (currentGrenade)
@@ -39,7 +51,7 @@ public class RandomSpawner : MonoBehaviour
             // Instantiate the shield at the same spawn point's position
             currentShield = Instantiate(shieldPrefab, randomSpawnPoint.position, Quaternion.identity);
 
-            yield return new WaitForSeconds(10f);  // Grenade and shield stay for 10 seconds
+            yield return new WaitForSeconds(15f);  // Grenade and shield stay for 10 seconds
 
             // Destroy the grenade and shield
             if (currentGrenade)
